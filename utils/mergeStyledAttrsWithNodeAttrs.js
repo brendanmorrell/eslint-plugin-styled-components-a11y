13ae2f49f96b098cfef8496a2686d574feb9b2f0
brendan <brendan.morrell@medmen.com>
2020-02-07 22:26:06 -0800

    added estree to parser so it runs ast conversions just like eslint

--- utils/mergeStyledAttrsWithNodeAttrs.js
+++ utils/mergeStyledAttrsWithNodeAttrs.js
@@ -2,7 +2,7 @@ const { parse } = require('@babel/parser');
 
 module.exports = (styledAttrs, nodeAttrs) => {
   const jsxString = `<div ${styledAttrs.map(keyValuePairToProps)}/>`;
-  const ast = parse(jsxString, { plugins: ['jsx'] });
+  const ast = parse(jsxString, { plugins: ['jsx', 'estree'] });
   const astAttributes = ast.program.body[0].expression.openingElement.attributes;
   return nodeAttrs.concat(astAttributes);
 };
