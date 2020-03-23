1b178971877d0ae5ef70d7f2e5d6baacfa5812f7
Brendan Morrell <brendanmorrell@gmail.com>
2020-03-22 18:18:44 -0700

    couple more keywords

--- dist/utils/keyValuePairToProps.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports = function (_ref) {
-  var key = _ref.key,
-      value = _ref.value;
-  return "".concat(key, "=").concat(typeof value === 'string' ? "\"".concat(value, "\"") : "{".concat(value, "}"), " ");
-};
