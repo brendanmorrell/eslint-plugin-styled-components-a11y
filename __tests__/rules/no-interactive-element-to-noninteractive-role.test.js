32315411d00452fad16bb3199f3a10d7143b9ffd
Brendan Morrell <brendanmorrell@gmail.com>
2020-03-24 15:19:42 -0700

    non-interactive test

---
+++ __tests__/rules/no-interactive-element-to-noninteractive-role.test.js
@@ -0,0 +1,36 @@
+const { RuleTester } = require('eslint');
+const makeRule = require('../../src/utils/makeRule');
+const parserOptionsMapper = require('../utils/parserOptionsMapper');
+const getSuggestion = require('../utils/getSuggestion.js');
+
+const ruleTester = new RuleTester();
+const makeStyledTestCases = require('../utils/makeStyledTestCases');
+
+const ruleName = 'no-interactive-element-to-noninteractive-role';
+const rule = makeRule(ruleName);
+
+const errorMessage = 'Interactive elements should not be assigned non-interactive roles.';
+
+// ## VALID
+// <div role="article">
+//   <button />;
+// </div>;
+const divArticleRoleWrapperButton = makeStyledTestCases({
+  attrs: `{ role: 'article' }`,
+  props: 'role="article"',
+  children: '<button />',
+});
+
+// ## INVALID
+// <button role="article">Save</button>
+const buttonArticleRole = makeStyledTestCases({
+  tag: 'button',
+  attrs: `{ role: 'article' }`,
+  props: 'role="article"',
+  errors: [errorMessage],
+});
+
+ruleTester.run(ruleName, rule, {
+  valid: [...divArticleRoleWrapperButton],
+  invalid: [buttonArticleRole[1]],
+});
