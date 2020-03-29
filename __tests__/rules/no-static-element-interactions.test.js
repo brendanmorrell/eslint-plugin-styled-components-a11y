2eab05291d0afa135a5e7c4d292337fce85b1ee3
Brendan Morrell <brendanmorrell@gmail.com>
2020-03-29 15:24:32 -0600

    no-static-element-interactions

---
+++ __tests__/rules/no-static-element-interactions.test.js
@@ -0,0 +1,43 @@
+const { RuleTester } = require('eslint');
+const makeRule = require('../../src/utils/makeRule');
+const parserOptionsMapper = require('../utils/parserOptionsMapper');
+const getSuggestion = require('../utils/getSuggestion.js');
+
+const ruleTester = new RuleTester();
+const makeStyledTestCases = require('../utils/makeStyledTestCases');
+
+const ruleName = 'no-static-element-interactions';
+const rule = makeRule(ruleName);
+
+const expectedError = 'Static HTML elements with event handlers require a role.';
+
+// ## VALID
+// <button onClick={() => 0} className="foo" />
+const button = makeStyledTestCases({
+  tag: 'button',
+  attrs: "{ onClick:() => 0, className:'foo' }",
+  props: 'onClick={() => 0} className="foo"',
+});
+// <div className="foo" onClick={() => 0} role="button" />
+const divRoleButton = makeStyledTestCases({
+  attrs: "{ onClick:() => 0, className:'foo', role:'button' }",
+  props: 'onClick={() => 0} className="foo" role="button"',
+});
+// <input type="text" onClick={() => 0} />
+const input = makeStyledTestCases({
+  tag: 'input',
+  attrs: "{ onClick:() => 0, type:'text' }",
+  props: 'type="text" onClick={() => 0}',
+});
+// ## INVALID
+// <div onClick={() => 0} />
+const divOnClick = makeStyledTestCases({
+  attrs: '{ onClick:() => 0 }',
+  props: 'onClick={() => 0}',
+  errors: [expectedError],
+});
+
+ruleTester.run(ruleName, rule, {
+  valid: [...button, ...divRoleButton, ...input],
+  invalid: divOnClick,
+});
