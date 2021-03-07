03c996009afa067f8342f9a21ba24dfcf296a3c0
Brendan Morrell <brendanmorell@gmail.com>
2021-03-06 22:07:19 -0800

    added parser options to handle optional chaining

--- playground/.eslintrc.js
+++ playground/.eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   plugins: ['react', 'styled-components-a11y'],
   extends: ['eslint-config-airbnb', 'prettier', 'prettier/react', 'plugin:styled-components-a11y/recommended'],
+  parserOptions: {
+    "ecmaVersion": 2020,
+    parser: 'babel-eslint',
+    sourceType: 'module',
+  },
   env: {
     es6: true,
     browser: true,
